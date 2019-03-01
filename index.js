b95f67de10b8bac00d003e672780541c60c60816
Brendan Howell <bhowell841@gmail.com>
2019-02-28 19:17:54 -0500

    minor fixes

--- index.js
+++ index.js
@@ -3,39 +3,37 @@ var Word = require("./word.js");
 
 var testWord = new Word("cat");
 
-var questions = [
-    {
-        message: "Guess a letter?",
-        type: "input",
-        name: "letter"
-    }];
-    
-    function askQue(){
-        displayWord();
-        inquirer.prompt(questions).then(function(answer){
-            // console.log(answer);
-            // console.log(testWord);
-            for (var i = 0; i < testWord.wordArray.length; i++){
-                if(answer.letter === testWord.wordArray[i].char){
-                    testWord.wordArray[i].showLetter = true;
-                }
-            }
-        askQue()
-        })
-    }
+var questions = [{
+    message: "Guess a letter?",
+    type: "input",
+    name: "letter"
+}];
 
-    function displayWord(){
-        var cleanStr = "";
-        console.log(testWord)
-        for (var i = 0; i < testWord.wordArray.length; i++){
-            if (testWord.wordArray[i].showLetter === true){
-                cleanStr += testWord.wordArray[i].char; 
-            }
-            else {
-                cleanStr += "_ ";
+function askQue() {
+    displayWord();
+    inquirer.prompt(questions).then(function (answer) {
+        // console.log(answer);
+        // console.log(testWord);
+        for (var i = 0; i < testWord.wordArray.length; i++) {
+            if (answer.letter === testWord.wordArray[i].char) {
+                testWord.wordArray[i].showLetter = true;
             }
         }
-        console.log(cleanStr);
-    }
+        askQue()
+    })
+}
 
-    askQue();
+function displayWord() {
+    var cleanStr = "";
+    console.log(testWord)
+    for (var i = 0; i < testWord.wordArray.length; i++) {
+        if (testWord.wordArray[i].showLetter === true) {
+            cleanStr += testWord.wordArray[i].char;
+        } else {
+            cleanStr += "_ ";
+        }
+    }
+    console.log(cleanStr);
+}
+
+askQue();
